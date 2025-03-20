5c48a62b9b553f4d5c90e2d5fbc1d0b92a736fc0
willp34 <35201865+willp34@users.noreply.github.com>
2025-03-20 21:55:37 +0000

    Add files via upload

--- src/Orders.php
+++ src/Orders.php
@@ -4,6 +4,12 @@
 	   
 	   private $no_orders =0;
 	   private  $shopify_orders = array();
+	   
+	   /*
+	   * The constructor will firstly get the content of the json  file an turn it into an array. 
+	   * If error 500 is generated an exception will be thrown.
+	   * If no Erros are found calculate the number of orders along with the average cost of sales 
+	   */
 	   public  function __construct(){
 					
 				$url='https://fauxdata.codelayer.io/api/orders';
@@ -12,18 +18,22 @@
 				if ($json === false) {
 					throw new Exception("Failed to retrieve data from $url");
 				
-					exit;
+					
 				}
 				$this->shopify_orders =json_decode($json,true);
-				
+				$this->no_orders= count($this->shopify_orders["orders"]);
 				$this->display();
 		   
 	   }
 	   
+	   /*
+	   * Calculate The revenue by looping throught the shopify_orders["orders"] and fing key price 
+	   * within Items array and add them all up
+	   * @return float
+	   */
 	   private function cost_of_revenue(){
 		   
-		   $key = "price";
-		    $revenue = 0.00;
+		   $revenue = 0.00;
 		   foreach($this->shopify_orders["orders"] as $order){
 			  
 			   foreach($order['items'] as $product){
@@ -34,19 +44,28 @@
 		   
 	   }
 	   
+	   /*
+	   *   Revenue /number of Orders 
+	   *  @return String
+	   */
+	   
 	   private function getAverageOrderValue(){
-		
+
+		 // Prevent division by zero
 		if ($this->no_orders === 0) {
-            return "0.00";  // Prevent division by zero
+            return "0.00"; 
         }
 
 		   $average_order_value =  $this->cost_of_revenue()/$this->no_orders;
 		   return number_format($average_order_value,2);
 		   
 	   }
-	   
+	   /*
+	   *  Display the Average order value to the user
+	   * 	Void
+	   */
 	   private function display(){
-		   $this->no_orders= count($this->shopify_orders["orders"]);
+				
 				//echo "<br  />orders  :  ". $this->no_orders; 
 				//echo "<br  />Revenue  :  £".  number_format($this->cost_of_revenue(), 2);
 				echo "Average order Value  :  £". $this->getAverageOrderValue();
